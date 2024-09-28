e2a7a2d161db014cf7687d93cb080eefc1596784
fatimahnaeem <kk301659@gmail.com>
2024-09-28 18:50:45 +0500

    code

---
+++ script.js
@@ -0,0 +1,11 @@
+var num = prompt("Enter your name")
+var num1 = prompt("Enter your number")
+var num2 = prompt("Enter your email")
+var num3 = prompt("Enter your pass")
+
+let array =[]
+    let hania = prompt ("enter your name")
+         
+    array.push(hania)
+
+    console.log(array)
